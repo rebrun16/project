9fa7ac3a4ad38f436a78f59f23165bd4bf136aa8
Vitalii <rebrun508@gmail.com>
2023-02-07 22:08:27 +0200

    work with new branch

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
 
+import React from "react";
+import "./App.css";
+
+import App1 from "./components/App1";
+
+
 function App() {
   return (
-    <div className="App">
-      <h1>HELLO</h1>
+    <div className="container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10">
+      <p className="text-3xl text-gray-700 font-bold mb-5">
+        Welcome!
+      </p>
+      <h1 className="text-3xl font-bold underline text-red-600">
+      Simple React Typescript Tailwind Sample
+      </h1>
+      <App1/>
     </div>
   );
 }
